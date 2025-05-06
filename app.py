fe77b00c5ac145da8f6853e619c1860ea69ee109
Joviel <110842713+Jovi38@users.noreply.github.com>
2025-05-06 10:09:09 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,9 +4,8 @@ import numpy as np
 import joblib
 
 # Load model and scaler
-model = joblib.load('best_churn_model.pkl')
-scaler = joblib.load('scaler.pkl')
-
+model = joblib.load("best_churn_model.pkl")
+scaler = joblib.load("scaler.pkl")
 st.title("Customer Churn Prediction Dashboard")
 
 st.write("Enter customer details to predict churn:")
